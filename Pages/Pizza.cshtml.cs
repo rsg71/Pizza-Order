b0e5f7f0558819cfafbc8ce2ea7bd24011317e98
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-08-24 22:33:33 -0400

    Initial commit

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
+
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+        public List<Pizza> pizzas = new();
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+    }
+}
